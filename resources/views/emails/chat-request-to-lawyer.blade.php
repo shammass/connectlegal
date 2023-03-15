4275480d0c53a35336c32ba74a484f98c7c65501
Shammas T A <shammas@Shammass-Air.domain.name>
2023-03-15 10:53:49 +0530

    fixing

--- resources/views/emails/chat-request-to-lawyer.blade.php
+++ resources/views/emails/chat-request-to-lawyer.blade.php
@@ -5,7 +5,7 @@
 <p>The user has expressed interest in seeking legal advice and believes that you can assist them with your expertise in {{$area}}.</p>
 
 <p>We encourage you to respond to the chat request at your earliest convenience and provide the user with your expert legal advice. To access the chat session, please log in to your account on Connect Legal and go to the Chat section. You can find the chat with {{$userName}} under the Online Chat Requests tab or click on the link below:</p>
-<a href="https://dev.test.connectlegal.ae/lawyer/online-chat-requests"></a>
+<a href="https://dev.test.connectlegal.ae/lawyer/online-chat-requests">View</a>
 
 
 <hr>
